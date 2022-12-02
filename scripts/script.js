1783e54fb26d7f75a94d0fa47251fd7934b1cca8
Anton31100 <100073869+Anton31100@users.noreply.github.com>
2022-12-02 22:38:41 +0300

    html

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+    var x = parseInt(prompt(""));
+    var y = parseInt(prompt(""));
+    
+    res = x + y;
+    
+    document.getElementById('x').innerText=x;
+    document.getElementById('y').innerText=y;
+    document.getElementById('res').innerText=res;
+    
+  
